b3149d1e68fefb190925bc6296029b5f7358ffe1
patiltrade2002-svg <patil.trade2002@gmail.com>
2026-01-13 17:15:06 +0100

    Update coinbase.py

--- coinbase.py
+++ coinbase.py
@@ -1,9 +1,19 @@
 import json
 import websockets
 from feeds.shared import PRICES
-from feeds.config import COINS
 
-PRODUCT_MAP = {v["coinbase"]: k for k, v in COINS.items()}
+PRODUCTS = {
+    "BTC-USD": "BTC",
+    "ETH-USD": "ETH",
+    "SOL-USD": "SOL",
+    "ADA-USD": "ADA",
+    "XRP-USD": "XRP",
+    "DOGE-USD": "DOGE",
+    "LINK-USD": "LINK",
+    "AVAX-USD": "AVAX",
+    "MATIC-USD": "MATIC",
+    "DOT-USD": "DOT",
+}
 
 async def run_coinbase():
     uri = "wss://ws-feed.exchange.coinbase.com"
@@ -11,16 +21,12 @@ async def run_coinbase():
     async with websockets.connect(uri) as ws:
         await ws.send(json.dumps({
             "type": "subscribe",
-            "channels": [{
-                "name": "ticker",
-                "product_ids": list(PRODUCT_MAP.keys())
-            }]
+            "channels": [{"name": "ticker", "product_ids": list(PRODUCTS)}]
         }))
 
         while True:
             msg = json.loads(await ws.recv())
-
             if msg.get("type") == "ticker":
-                coin = PRODUCT_MAP.get(msg["product_id"])
+                coin = PRODUCTS.get(msg["product_id"])
                 if coin:
                     PRICES[coin]["Coinbase"] = float(msg["price"])
